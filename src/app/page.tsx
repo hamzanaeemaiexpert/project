0922beab950268429dee81af2e74c1365cba566c
Hamza Naeeem <hamzanaeemaiexpert@gmail.com>
2024-09-22 23:05:51 +0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>export default function Home() {
-      (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-      <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/hamza.jpg.jpeg" alt="" width="384" height="512"/>
-      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p className="text-lg font-medium">
-           I m Hamza a rising talent in the field of Artificial Intelligence. As a successful entrepreneur I hve founded and led my own marketing agency to remarkable success. I m constantly seeking synergies between AI and marketing to propel businesses forward."
-          </p>
-        </blockquote>
-        <figcaption className="font-medium">
-          <div className="text-sky-900 dark:text-sky-400">
-            Hamza Naeem
-          </div>
-          <div className="text-slate-700 dark:text-slate-500">
-            Ai Expert
-          </div>
-        </figcaption>
-      </div>
-    </figure></div>
-          </main>
-      )
-    }
-    
+    <div>Home page
+      <blockquote>
+        <p className="flex min-h-screen flex-col items-center justify-between p-24 text-lg font-medium">
+
+        </p>
+      </blockquote>
     </div>
   )
 }
 
 export default page
-
