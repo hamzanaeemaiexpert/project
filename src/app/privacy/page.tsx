f178e903c23b808a486199a32ad5ad8c3ef01e97
Hamza Naeeem <hamzanaeemaiexpert@gmail.com>
2024-09-22 23:17:08 +0500

    ok

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -5,7 +5,7 @@ export const page = () => {
     <div>privacy page
         <blockquote>
             <p className="flex min-h-screen flex-col items-center justify-between p-24 text-lg font-medium">
-             Copyright © 2024 All rights reserved. Powered and Manage By Microdest Pvt. LTD
+             Copyright 2024 All rights reserved. Powered and Manage By Microdest Pvt. LTD
             </p>
         </blockquote>
     </div>
