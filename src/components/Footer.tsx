2527c1d699e165acad33de5135994fd7b0d8d4ea
Hamza Naeeem <hamzanaeemaiexpert@gmail.com>
2024-09-24 14:32:49 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <div className='flexd bottom-0 left-0 flex justify-center bg-slate-400 gap-4 p-5 w-full'>
-       <Link href="\privacy">privacy page</Link>
        <Link href="\terms">Terms page</Link>
     </div>
  )
